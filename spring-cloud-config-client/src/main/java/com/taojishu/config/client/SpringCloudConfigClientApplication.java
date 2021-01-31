7e134feb4a2fabe58c905464f031198664f17954
taojishu <1257060204@qq.com>
2021-01-31 16:40:34 +0800

    Spring Cloud Config

--- spring-cloud-config-client/src/main/java/com/taojishu/config/client/SpringCloudConfigClientApplication.java
+++ spring-cloud-config-client/src/main/java/com/taojishu/config/client/SpringCloudConfigClientApplication.java
@@ -3,9 +3,18 @@ package com.taojishu.config.client;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringCloudConfigClientApplication {
+
+	@RequestMapping("/")
+	public String home() {
+		return "Hello World!";
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudConfigClientApplication.class, args);
